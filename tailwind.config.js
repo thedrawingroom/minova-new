b029acf3cdfe65ef251570a30174e4f24603bf60
Chris Laine <laine.chris1@gmail.com>
2024-10-30 14:11:55 +0000

    Update editor configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,24 +34,16 @@ export default {
                 button: '25px',
             },
             screens: {
+        '3xl': '2100px',
                 '2xl': '1440px',
                 xl: '1140px',
                 lg: '960px',
                 md: '720px',
                 sm: '540px',
             },
-            safelist: [
-                'sm:text-xl',
-                'text-4xl',
-                'sm:text-6xl',
-                'text-7xl',
-                'bg-blue-500',
-                'sm:bg-red-500',
-            ],
+      safelist: ['sm:text-xl', 'text-4xl', 'sm:text-6xl', 'text-7xl', 'bg-blue-500', 'sm:bg-red-500'],
         },
     },
 
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+  plugins: [require('@tailwindcss/typography')],
 };
