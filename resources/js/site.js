febb849a669117b375713beebd49aaa50e8ef3d1
Matt Heywood <mheywood90@gmail.com>
2024-11-04 16:50:44 +0000

    content, page columns and accordion block

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,7 +1,14 @@
 // This is all you.
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
 
 // Component files
 import sliders from './components/sliders';
+
+// Alpine
+window.Alpine = Alpine;
+Alpine.plugin(collapse);
+Alpine.start();
 
 // Sliders
 sliders.init();
