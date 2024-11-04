ba834cd1837d67891513326b92e5f62a4960707c
Matt Heywood <mheywood90@gmail.com>
2024-11-04 15:46:50 +0000

    Amends to multiple blocks

--- resources/js/components/sliders.js
+++ resources/js/components/sliders.js
@@ -36,7 +36,7 @@ export default {
 
     if (this.sliderProductEl.length) {
       this.sliderProductEl.slick({
-        slidesToShow: 5,
+        slidesToShow: 4,
         slidesToScroll: 1,
         arrows: true,
         dots: false,
@@ -49,19 +49,21 @@ export default {
           {
             breakpoint: 1440,
             settings: {
-              slidesToShow: 4,
+              slidesToShow: 3,
             },
           },
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
+              slidesToShow: 2,
+              dots: true,
+              arrows: false,
             },
           },
           {
-            breakpoint: 960,
+            breakpoint: 768,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 1,
               dots: true,
               arrows: false,
             },
