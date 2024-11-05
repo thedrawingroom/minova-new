7f77ea03e3512952af31d79f779e82d3aa9583fe
Matt Heywood <mheywood90@gmail.com>
2024-11-05 14:57:57 +0000

    Fix letters function

--- resources/js/components/letters.js
+++ resources/js/components/letters.js
@@ -1,18 +1,16 @@
 export default {
   init() {
     $('.title-split').each(function (index, element) {
-      var heading = $(element);
-      var word_array, last_word, first_part;
+      const heading = $(element);
+      const text = $(element).html();
+      const segmenter = new Intl.Segmenter([], { granularity: 'word' });
+      const segmentedText = segmenter.segment(text);
+      const words = [...segmentedText].filter(s => s.isWordLike).map(s => s.segment);
+      const length = words.length;
 
-      word_array = heading.html().split(/\s+/); // split on spaces
-      console.log(word_array.length);
-      last_word = word_array.pop(); // pop the last word
-      first_part = word_array.join(' '); // rejoin the first words together
-
-      if (word_array.length > 1) {
-        heading.html([first_part, ' <span style="color: #FAB90A;">', last_word, '</span>'].join(''));
-      } else {
-        heading.html([first_part, last_word].join(''));
+      if (length > 1) {
+        const lastItem = ' <span style="color: #FAB90A;">' + words.splice(-1) + '</span>';
+        heading.html([words, lastItem].join(''));
       }
     });
   },
