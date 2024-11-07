370251dc1af54766e9791824f72b93ca03377c31
Matt Heywood <mheywood90@gmail.com>
2024-11-07 15:21:47 +0000

    Minor styling fixes

---
+++ resources/js/includes/arrows.js
@@ -0,0 +1,27 @@
+export const prevArrowLarge = `
+<button class="slick-arrow slick-arrow--prev absolute left-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 192 192"
+    class="size-10 rotate-180"
+  >
+    <polygon
+      class="fill-minova-gold"
+      points="17.74 14.43 11.06 28.16 159.02 94.54 12.54 163.88 19.23 177.6 180.9 101.21 180.9 88.6 17.74 14.43"
+    />
+  </svg>
+</button>`;
+
+export const nextArrowLarge = `
+<button class="slick-arrow slick-arrow--next absolute right-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block">
+   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 192 192"
+    class="size-10"
+  >
+    <polygon
+      class="fill-minova-gold"
+      points="17.74 14.43 11.06 28.16 159.02 94.54 12.54 163.88 19.23 177.6 180.9 101.21 180.9 88.6 17.74 14.43"
+    />
+  </svg>
+</button>`;
--- resources/js/includes/sliders.js
+++ resources/js/includes/sliders.js
@@ -1,35 +1,4 @@
-const prevArrowProject = `
-<button class="slick-arrow slick-arrow--prev absolute left-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 192 192"
-    class="size-10 rotate-180"
-  >
-    <polygon
-      class="fill-minova-gold"
-      points="17.74 14.43 11.06 28.16 159.02 94.54 12.54 163.88 19.23 177.6 180.9 101.21 180.9 88.6 17.74 14.43"
-    />
-  </svg>
-</button>`;
-
-const nextArrowProject = `
-<button class="slick-arrow slick-arrow--next absolute right-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block">
-   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 192 192"
-    class="size-10"
-  >
-    <polygon
-      class="fill-minova-gold"
-      points="17.74 14.43 11.06 28.16 159.02 94.54 12.54 163.88 19.23 177.6 180.9 101.21 180.9 88.6 17.74 14.43"
-    />
-  </svg>
-</button>`;
-
-const prevArrowInvert =
-  '<button class="slick-arrow slick-arrow--prev absolute left-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block"><i class="fa-regular fa-chevron-left text-white"></i></button>';
-const nextArrowInvert =
-  '<button class="slick-arrow slick-arrow--next absolute right-0 top-1/2 -translate-y-1/2 text-3xl 3xl:text-5xl px-3 inline-block"><i class="fa-regular fa-chevron-right text-white"></i></button>';
+import { prevArrowLarge, nextArrowLarge } from '../includes/arrows';
 
 export default {
   init() {
@@ -37,11 +6,15 @@ export default {
     this.sliderProductEl = $('.product-slider');
 
     $('.accordion-control').on('click', () => {
-      $(window).trigger('resize');
+      setTimeout(() => {
+        $(window).trigger('resize');
+      }, 100);
     });
 
-    addEventListener('resize', function () {
-      $('.slick-slider').slick('resize');
+    window.addEventListener('resize', () => {
+      $('.slick-slider').each(function () {
+        $(this).slick('resize');
+      });
     });
 
     if (this.sliderProjectEl.length) {
@@ -50,8 +23,8 @@ export default {
         slidesToScroll: 1,
         arrows: true,
         dots: false,
-        prevArrow: prevArrowProject,
-        nextArrow: nextArrowProject,
+        prevArrow: prevArrowLarge,
+        nextArrow: nextArrowLarge,
         rows: 0,
         autoplay: false,
         autoplaySpeed: 5000,
@@ -75,8 +48,8 @@ export default {
         slidesToScroll: 5,
         arrows: true,
         dots: false,
-        prevArrow: prevArrowInvert,
-        nextArrow: nextArrowInvert,
+        prevArrow: prevArrowLarge,
+        nextArrow: nextArrowLarge,
         rows: 0,
         autoplay: false,
         autoplaySpeed: 5000,
