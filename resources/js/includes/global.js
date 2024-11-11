b84a119835460a03de1297634272dba798e7b1a7
greenraedr <rae@thedrawingroomcreative.com>
2024-11-11 14:32:52 +0000

    Curretn work

--- resources/js/includes/global.js
+++ resources/js/includes/global.js
@@ -13,5 +13,28 @@ export default {
     $('.offcanvas-toggle').on('click', () => {
       $('body').toggleClass('offcanvas-open');
     });
+
+    const initRegionSelector = () => {
+      const regionSelector = document.getElementById('region-selector');
+      if (!regionSelector) return;
+
+      const currentPath = window.location.pathname;
+
+      if (currentPath.includes('/americas')) {
+        regionSelector.value = 'americas';
+      } else if (currentPath.includes('/emea-cis')) {
+        regionSelector.value = 'emea-cis';
+      } else if (currentPath.includes('/apac')) {
+        regionSelector.value = 'apac';
+      }
+
+      regionSelector.addEventListener('change', function () {
+        const selectedOption = this.options[this.selectedIndex];
+        const redirectUrl = selectedOption.getAttribute('data-url');
+        window.location.href = redirectUrl;
+      });
+    };
+
+    initRegionSelector();
   },
 };
