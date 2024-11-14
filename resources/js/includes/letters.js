82cdea40f51ba1ca506c21249a746075f727c0f9
greenraedr <rae@thedrawingroomcreative.com>
2024-11-14 12:32:19 +0000

    Change to the Hero Banner JS
    The JS file that divided up the hero banner was removing the symbols of any product that had more than 1 word in the title. This should fix that issue now.

--- resources/js/includes/letters.js
+++ resources/js/includes/letters.js
@@ -3,15 +3,31 @@ export default {
     $('.title-split').each(function (index, element) {
       const heading = $(element);
       const text = $(element).html();
+      
       const segmenter = new Intl.Segmenter([], { granularity: 'word' });
       const segmentedText = segmenter.segment(text);
-      const words = [...segmentedText].filter(s => s.isWordLike).map(s => s.segment);
-      const length = words.length;
+      
+      const words = [...segmentedText];
+      const length = words.filter(s => s.isWordLike).length;
 
       if (length > 1) {
-        const lastItem = ['<span style="color: #FAB90A;"> ' + words.splice(-1) + '</span>'];
-        const allWords = words.concat(lastItem);
-        heading.html(allWords.join(' '));
+        let lastWordIndex = -1;
+
+        for (let i = words.length - 1; i >= 0; i--) {
+          if (words[i].isWordLike) {
+            lastWordIndex = i;
+            break;
+          }
+        }
+
+        const updatedSegments = words.map((segment, index) => {
+          if (index === lastWordIndex) {
+            return `<span style="color: #FAB90A;">${segment.segment}</span>`;
+          }
+          return segment.segment;
+        });
+
+        heading.html(updatedSegments.join(''));
       }
     });
   },
