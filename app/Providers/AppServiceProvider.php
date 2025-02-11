6191ac33bd8ce8e291b43461801c3c92003a1bb8
Lee Millward <lee@m36.co.uk>
2025-02-11 14:23:12 +0000

    Force HTTPS on production and staging.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
 
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS on production and staging
+        if ($this->app->environment('production', 'staging')) {
+            URL::forceScheme('https');
+        }
+
         // Statamic::vite('app', [
         //     'resources/js/cp.js',
         //     'resources/css/cp.css',
