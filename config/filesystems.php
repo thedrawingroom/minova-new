e7a4197455dbbf45b9a0e6e9215b908489255b0a
Lee Millward <lee@m36.co.uk>
2025-02-12 09:53:58 +0000

    Use Azure storage driver directly for assets.

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,11 +67,6 @@ return [
         ],
 
         'assets' => [
-            'driver' => 'alias',
-            'target' => env('STATAMIC_ASSETS_DISK', 'azure')
-        ],
-
-        'azure' => [
             'driver' => 'azure-storage-blob',
             'connection_string' => env('AZURE_STORAGE_CONNECTION_STRING'),
             'container' => env('AZURE_STORAGE_CONTAINER'),
