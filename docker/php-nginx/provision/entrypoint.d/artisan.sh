3e254e8a8da113fd8c9c31dc323f9b69bce82119
Lee Millward <lee@m36.co.uk>
2025-02-18 08:17:00 +0000

    Remove SSH start command.

--- docker/php-nginx/provision/entrypoint.d/artisan.sh
+++ docker/php-nginx/provision/entrypoint.d/artisan.sh
@@ -5,9 +5,6 @@ set -e
 # Get env vars in the Dockerfile to show up in the SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
-echo "Starting SSH ..."
-service ssh start
-
 mkdir -p /app/storage/logs/workers
 mkdir -p /app/storage/framework/cache/data
 mkdir -p /app/storage/framework/sessions
